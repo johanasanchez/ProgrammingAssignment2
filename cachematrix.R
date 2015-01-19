c29f2414358cd13c74782093f43b1c0ac1451347
johanasanchez <johanasanchez@users.noreply.github.com>
2015-01-19 14:28:01 -0430

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,6 +4,12 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+        matinv <- NULL
+        set <- function(y) {
+                x <<- y
+                matinv <<- NULL
+        }
+        
 
 }
 
